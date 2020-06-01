ba3d196fbd566d4ba09fb57a563b9245e265618e
George Gayno <George.Gayno@noaa.gov>
2020-06-01 18:39:09 +0000

    master:
    Initial commit of v2.0.8 of wgrib2.

---
+++ README.md
@@ -0,0 +1,1 @@
+WGRIB2 library repository.
---
+++ grib2/wgrib2/config.h
@@ -0,0 +1,21 @@
+/* config.h */
+#define USE_REGEX
+#define USE_TIGGE
+//#define DISABLE_ALARM
+//#define DISABLE_TIMEZONE
+//#define USE_UDF
+#define IPOLATES_LIB "ip2lib_d"
+#define USE_IPOLATES 3
+#define USE_OPENMP
+//#define USE_G2CLIB
+//#define USE_PROJ4
+#define USE_JASPER
+#define USE_AEC "libaec-1.0.2.tar.gz"
+#define USE_NETCDF3
+//#define USE_NETCDF4
+//#define USE_MYSQL
+#define CC "gcc (GCC) 4.4.7 20120313 (Red Hat 4.4.7-23)"
+#define FORTRAN "GNU Fortran (GCC) 4.4.7 20120313 (Red Hat 4.4.7-23)"
+#define BUILD_COMMENTS "stock build"
+#define USE_PNG
+//#define WMO_VALIDATION
