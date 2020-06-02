55a75c3d80fb398a8dd1c0991d8dfecad1d07516
George Gayno <George.Gayno@noaa.gov>
2020-06-02 14:00:11 +0000

    feature/v2.0.8  This commit references #1.
    Update to build on Jet.

--- compile.sh
+++ compile.sh
@@ -8,23 +8,29 @@
 
 set -x
 
-machine=$(cat /etc/prod)
+machine=$1
 echo $machine
 
-if [ $machine = 'mars' ] || [ $machine = 'venus' ]; then
+if [ $machine = 'dell' ]; then
   module purge
   module load ips/18.0.1.163 
   module list
   export CC=icc
   export FC=ifort
   export COMP_SYS=intel_linux
-elif [ $machine = 'luna' ] || [ $machine = 'surge' ]; then
+elif [ $machine = 'cray' ]; then
   echo cray
   module purge
   module load PrgEnv-intel/5.2.56
   module rm intel
   module load intel/16.3.210
   module load craype-haswell
+  export CC=icc
+  export FC=ifort
+  export COMP_SYS=intel_linux
+elif [ $machine = 'jet' ]; then
+  module purge
+  module load intel/18.0.5.274
   export CC=icc
   export FC=ifort
   export COMP_SYS=intel_linux
