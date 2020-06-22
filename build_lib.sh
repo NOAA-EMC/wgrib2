42ffdfd4213d52939018787a3233e763a8e0fc63
George Gayno <George.Gayno@noaa.gov>
2020-06-22 20:08:15 +0000

    feature/cmake  This commit references #1.
    Update library build script for Dell.

--- build_lib.sh
+++ build_lib.sh
@@ -17,6 +17,12 @@ if [ $machine = 'hera' ]; then
   module load intel/18.0.5.274
   export CC=icc
   export FC=ifort
+elif [ $machine = 'dell' ]; then
+  module purge
+  module load cmake
+  module load ips/18.0.1.163
+  export CC=icc
+  export FC=ifort
 else
   set +x
   echo
