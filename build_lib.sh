9c3a3e6b008e7e435f5b999a1f63130c209473fd
George Gayno <George.Gayno@noaa.gov>
2020-06-22 19:46:26 +0000

    feature/cmake  This commit references #1.
    Add basic build script so users can build the library/api.
    Works only for Hera currently.

---
+++ build_lib.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+if [ $# -lt 1 ]; then
+  set +x
+  echo '***ERROR*** must specify machine name as argument.'
+  exit 1
+fi
+
+machine=$1
+echo $machine
+
+if [ $machine = 'hera' ]; then
+  module purge
+  module load cmake/3.16.1
+  module load intel/18.0.5.274
+  export CC=icc
+  export FC=ifort
+else
+  set +x
+  echo
+  echo
+  echo "ERROR: MACHINE " $machine " NOT SUPPORTED.  STOP."
+  exit 1
+fi
+
+rm -fr ./build
+mkdir -p ./build
+cd build
+
+cmake -DMAKE_FTN_API=ON -DUSE_NETCDF4=OFF -DUSE_PNG=OFF -DUSE_JASPER=OFF ../grib2
+
+make -j 8 VERBOSE=1
+
+make install
+
