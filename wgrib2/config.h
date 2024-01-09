7f756c0282b1c982e05a3aec5a2d9a59aad05327
Edward Hartnett <edward.hartnett@noaa.gov>
2024-01-09 10:47:50 -0700

    more changes in config.h

--- wgrib2/config.h
+++ wgrib2/config.h
@@ -9,8 +9,8 @@
 #define USE_IPOLATES 0
 //#define USE_OPENMP
 #define MAKE_SHARED_LIB
-//#define CPPFLAGS "-march=native -mtune=native -I/cpc/home/wd51we/grib2/include -Wall -Wmissing-prototypes -Wold-style-definition -Werror=format-security -O3 -DGFORTRAN -fopenmp -fPIC -g"
-//#define FFLAGS "-march=native -mtune=native -c -O3 -fopenmp -fPIC -g"
+#define CPPFLAGS "-march=native -mtune=native -I/cpc/home/wd51we/grib2/include -Wall -Wmissing-prototypes -Wold-style-definition -Werror=format-security -O3 -DGFORTRAN -fopenmp -fPIC -g"
+#define FFLAGS "-march=native -mtune=native -c -O3 -fopenmp -fPIC -g"
 //#define USE_G2CLIB
 //#define USE_PROJ4
 #define USE_JASPER
@@ -20,7 +20,6 @@
 //#define USE_MYSQL
 #define CC "gcc (GCC) 8.5.0 20210514 (Red Hat 8.5.0-18)"
 #define FORTRAN "GNU Fortran (GCC) 8.5.0 20210514 (Red Hat 8.5.0-18)"
-#define FFLAGS ""
 #define BUILD_COMMENTS "stock build"
 #define USE_PNG
 //#define WMO_VALIDATION
