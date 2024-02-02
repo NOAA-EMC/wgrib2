ebda98b2b1e16493187d83627977f9fb16985a84
webisu <wesley.ebisuzaki@noaa.gov>
2024-02-02 12:22:59 -0500

    add include/proj.h

--- wgrib2/config.h
+++ wgrib2/config.h
@@ -12,14 +12,14 @@
 #define CPPFLAGS "-march=native -mtune=native -I/cpc/home/wd51we/grib2/include -Wall -Wmissing-prototypes -Wold-style-definition -Werror=format-security -O3 -DGFORTRAN -fopenmp -fPIC -g"
 #define FFLAGS "-march=native -mtune=native -c -O3 -fopenmp -fPIC -g"
 //#define USE_G2CLIB
-#define USE_PROJ4
-#define USE_JASPER
+// #define USE_PROJ4
+// #define USE_JASPER
 //#define USE_AEC
-#define USE_SPECTRAL 1
-#define USE_NETCDF3
+// #define USE_SPECTRAL 1
+// #define USE_NETCDF3
 //#define USE_MYSQL
 #define CC "gcc (GCC) 8.5.0 20210514 (Red Hat 8.5.0-18)"
 #define FORTRAN "GNU Fortran (GCC) 8.5.0 20210514 (Red Hat 8.5.0-18)"
 #define BUILD_COMMENTS "stock build"
-#define USE_PNG
+// #define USE_PNG
 //#define WMO_VALIDATION
