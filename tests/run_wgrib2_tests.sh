2eb6e586541550a1c2353c43c8a98ff19c1e8853
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-27 08:01:33 -0600

    fixed first test

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -7,9 +7,10 @@ set -e
 echo ""
 echo "*** Running wgrib2 test"
 
-# Just run executable.
+# Just run executable. For some reason it returns 8.
 ls -l ../wgrib2
-#../wgrib2/wgrib2 > out.txt
+../wgrib2/wgrib2 > out.txt  && exit 1
+[ $? -ne 8 ] && exit 2
 
 echo "*** SUCCESS!"
 exit 0
