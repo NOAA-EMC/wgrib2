b206131a74522d70f3cee6741c0dec9ded4a7c62
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-27 06:12:19 -0600

    adding files

---
+++ tests/run_wgrib2_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a test script for the wgrib2 project.
+#
+# Ed Hartnett, 3/27/24
+
+set -e
+echo ""
+echo "*** Running wgrib2 test"
+
+# Just run executable.
+../wgrib2/wgrib2 > out.txt
+
+echo "*** SUCCESS!"
+exit 0
