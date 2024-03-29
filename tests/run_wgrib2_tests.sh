f69d196dee70f2e3598b1718268efdfef3dcf53b
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-29 09:22:28 -0600

    test inventory

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -15,8 +15,9 @@ ls -l ../wgrib2
 
 # Run an inventory on a test file. This does not work.
 #echo "*** testing inventory of gdaswave.t00z.wcoast.0p16.f000.grib2..."
-./wgrib2 ../tests/data/gdaswave.t00z.wcoast.0p16.f000.grib2 > tmp.txt
+../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 > tmp.txt
 cat tmp.txt
+cmp tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
 
 echo "*** SUCCESS!"
 exit 0
