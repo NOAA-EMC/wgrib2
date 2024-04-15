3220f4c9d754e8b47e010d9bfba56719c639be83
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-04-15 12:51:33 -0600

    Added a unit test

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -19,5 +19,10 @@ ls -l ../wgrib2
 cat tmp.txt
 cmp tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
 
+# Returns number of grid points in grid
+../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -npts > npts.txt
+cat npts.txt
+cmp npts.txt data/npts_gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+
 echo "*** SUCCESS!"
 exit 0
