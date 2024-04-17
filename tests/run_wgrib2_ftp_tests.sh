c80928e39b18082357d43564b956efdc129c854e
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-04-17 12:58:36 -0600

    Added FTP fetch and test

---
+++ tests/run_wgrib2_ftp_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is an optional test script for the wgrib2 project.
+#
+# Alyson Stahl, 4/17/24
+
+set -e
+echo ""
+echo "*** Running wgrib2 tests on FTP test files"
+
+# Run an inventory on a test file.
+../wgrib2/wgrib2 data/WW3_Regional_US_West_Coast_20220718_0000.grib2 > ftp_inv1.txt
+cat ftp_inv1.txt
+cmp ftp_inv1.txt data/ref_WW3_Regional_US_West_Coast_20220718_0000.grib2.inv
+
+echo "*** SUCCESS!"
+exit 0
