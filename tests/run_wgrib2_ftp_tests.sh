ae143a195e2d8588d9c898f8a383584396dd1361
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-04-18 09:02:38 -0600

    Added test for large data file

--- tests/run_wgrib2_ftp_tests.sh
+++ tests/run_wgrib2_ftp_tests.sh
@@ -7,10 +7,15 @@ set -e
 echo ""
 echo "*** Running wgrib2 tests on FTP test files"
 
-# Run an inventory on a test file.
+# Run an inventory on test files.
 ../wgrib2/wgrib2 data/WW3_Regional_US_West_Coast_20220718_0000.grib2 > ftp_inv1.txt
 cat ftp_inv1.txt
 cmp ftp_inv1.txt data/ref_WW3_Regional_US_West_Coast_20220718_0000.grib2.inv
 
+../wgrib2/wgrib2 data/rrfs.t18z.prslev.f000.grib2 > ftp_inv2.txt
+cat ftp_inv2.txt
+cmp ftp_inv2.txt data/ref_rrfs.t18z.prslev.f000.grib2.inv
+
 echo "*** SUCCESS!"
 exit 0
+
