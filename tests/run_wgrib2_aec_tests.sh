f84aae6f60b548323d173a50df63b9b2af174c53
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-04-18 13:10:12 -0600

    Update run_wgrib2_aec_tests.sh

--- tests/run_wgrib2_aec_tests.sh
+++ tests/run_wgrib2_aec_tests.sh
@@ -8,7 +8,7 @@ echo ""
 echo "*** Running wgrib2 tests"
 
 # Testing aec compression
-../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -set_grib_type -grib_out test.aec.grib2
+../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -set_grib_type aec -grib_out test.aec.grib2
 
 # Check grid data are identical
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -rpn sto_1 -import_grib test.aec.grib2 -rpn "rcl_1:print_rms" | grep -v "rpn_rms=0:" | wc -l
