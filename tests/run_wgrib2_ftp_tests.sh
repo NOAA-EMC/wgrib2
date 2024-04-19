eba4f72a3f392f5698d0d45dd05f411698c79537
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-04-19 08:34:53 -0600

    Removing large test file from test script for now

--- tests/run_wgrib2_ftp_tests.sh
+++ tests/run_wgrib2_ftp_tests.sh
@@ -12,9 +12,9 @@ echo "*** Running wgrib2 tests on FTP test files"
 cat ftp_inv1.txt
 cmp ftp_inv1.txt data/ref_WW3_Regional_US_West_Coast_20220718_0000.grib2.inv
 
-../wgrib2/wgrib2 data/rrfs.t18z.prslev.f000.grib2 > ftp_inv2.txt
-cat ftp_inv2.txt
-cmp ftp_inv2.txt data/ref_rrfs.t18z.prslev.f000.grib2.inv
+#../wgrib2/wgrib2 data/rrfs.t18z.prslev.f000.grib2 > ftp_inv2.txt
+#cat ftp_inv2.txt
+#cmp ftp_inv2.txt data/ref_rrfs.t18z.prslev.f000.grib2.inv
 
 echo "*** SUCCESS!"
 exit 0
