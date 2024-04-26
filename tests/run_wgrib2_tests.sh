fb4763e9ff57266ef1788e068b997a3427309e2e
AlysonStahl-NOAA <alyson.stahl@noaa.gov>
2024-04-26 10:49:25 -0600

    added tests and data files

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a test script for the wgrib2 project.
 #
-# Ed Hartnett, 3/27/24
+# Ed Hartnett, Alyson Stahl 3/27/24
 
 set -e
 echo ""
@@ -23,6 +23,22 @@ cmp tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -npts > npts.txt
 cat npts.txt
 cmp npts.txt data/ref_npts_gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+
+# Tests the calculation of wind speed, direction, and UGRD & VGRD components
+cksum_wind_0=`../wgrib2/wgrib2 data/ref_wind.grb -text - | cksum`
+cksum_uv_0=`../wgrib2/wgrib2 data/ref_uv.grb -text - | cksum`
+../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -wind_dir wind.grb -wind_speed wind.grb -match "(UGRD|VGRD)"
+cksum_wind_1=`../wgrib2/wgrib2 wind.grb -text - | cksum`
+if [ "$cksum_wind_0" != "$cksum_wind_1" ] ; then
+    echo "failed for computing wind speed and direction"
+    exit 1
+fi
+../wgrib2/wgrib2 wind.grb -wind_uv uv.grb
+cksum_uv_1=`../wgrib2/wgrib2 uv.grb -text - | cksum`
+if [ "$cksum_uv_0" != "$cksum_uv_1" ] ; then
+    echo "failed for computing UGRD & VGRD components"
+    exit 1
+fi
 
 echo "*** SUCCESS!"
 exit 0
