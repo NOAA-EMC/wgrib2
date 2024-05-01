24da50a34f3a1597b9cc8f032fae447818dc12c3
AlysonStahl-NOAA <alyson.stahl@noaa.gov>
2024-05-01 11:14:50 -0600

    enabled large test files

---
+++ tests/run_wgrib2_ftp_large_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This is an optional test script for the wgrib2 project. 
+# Requires FTP_LARGE_TEST_FILES=ON
+#
+# Alyson Stahl, 4/17/24
+
+set -e
+echo ""
+echo "*** Running wgrib2 tests on large FTP test files"
+
+echo "*** Running an inventory test on a large FTP test file"
+../wgrib2/wgrib2 data/rrfs.t18z.prslev.f000.grib2 > ftp_inv2.txt
+cat ftp_inv2.txt
+cmp ftp_inv2.txt data/ref_rrfs.t18z.prslev.f000.grib2.inv
+
+echo "*** SUCCESS!"
+exit 0
+
