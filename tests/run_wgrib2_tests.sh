4d4d16cbecc975d0b3f533c7faaa0f98c8c40521
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-01 21:59:39 -0600

    Adding more tests (#123)
    * added tests and data files
    
    * data files
    
    * added flags to workflows to use jpeg (required for tests)
    
    * correct flags to use jpeg packing
    
    * reverted changes
    
    * updated test with new data files in complex packing
    
    * trying simple packing to avoid potential jasper errors
    
    * changed test data file names and changed packing types
    
    * swapped checksum comparison for inventory comparison
    
    * fixed typo
    
    * missing file
    
    * another test
    
    * applied fix to developer workflow file

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a test script for the wgrib2 project.
 #
-# Ed Hartnett, 3/27/24
+# Ed Hartnett, Alyson Stahl 3/27/24
 
 set -e
 echo ""
@@ -23,6 +23,21 @@ cmp tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -npts > npts.txt
 cat npts.txt
 cmp npts.txt data/ref_npts_gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+
+# Tests the calculation of wind speed, direction, and UGRD & VGRD components
+../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -wind_dir wind.grb -wind_speed wind.grb -match "(UGRD|VGRD)"
+../wgrib2/wgrib2 wind.grb > wind.txt
+cat wind.txt
+cmp wind.txt data/ref_wind.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
+../wgrib2/wgrib2 wind.grb -wind_uv uv.grb
+../wgrib2/wgrib2 uv.grb > uv.txt
+cat uv.txt
+cmp uv.txt data/ref_uv.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
+
+# Tests printing of grid information
+../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -grid > grid_test.txt
+cat grid_test.txt
+cmp grid_test.txt data/ref_grid.gdaswave.t00z.wcoast.0p16.f000.grib2.txt
 
 echo "*** SUCCESS!"
 exit 0
