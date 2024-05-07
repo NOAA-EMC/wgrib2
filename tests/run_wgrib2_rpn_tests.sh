d19420b7e17c26c65e3cac362441006ea7fdc2e2
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-07 16:32:09 -0600

    more tests

--- tests/run_wgrib2_rpn_tests.sh
+++ tests/run_wgrib2_rpn_tests.sh
@@ -23,5 +23,18 @@ echo "*** Takes relative humidity values, limits them to 100 and outputs to grib
 touch rh.txt
 cmp rh.txt data/ref_rh.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
 
+echo "*** Calculates wind speed for records 1-25, then returns the average, min, and max values"
+../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -for 1:25 -match ":(UGRD|VGRD):" \
+    -if ":UGRD:" -rpn "sto_1" -fi \
+    -if ":VGRD:" -rpn "sto_2" -fi \
+    -if_reg 1:2 \
+        -rpn "rcl_1:sq:rcl_2:sq:+:sqrt:clr_1:clr_2" \
+        -set_var WIND \
+        -grib_out tmp_windspeed.grb
+
+../wgrib2/wgrib2.exe tmp_windspeed.grb -rpn print_min -rpn print_max -rpn print_ave > min_max_ave_windspeed.txt
+touch min_max_ave_windspeed.txt
+cmp min_max_ave_windspeed.txt data/ref_rpn.windspeed.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
+
 echo "*** SUCCESS!"
 exit 0
