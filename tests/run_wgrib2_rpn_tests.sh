740af43da0909faa11be7879e596592292796a88
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-07 15:09:37 -0600

    more file name typos

--- tests/run_wgrib2_rpn_tests.sh
+++ tests/run_wgrib2_rpn_tests.sh
@@ -13,9 +13,9 @@ touch celsius.txt
 cmp celsius.txt data/ref_celsius.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
 
 echo "*** Converting temperatures from K to F"
-../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -match ":TMP:0.01" -rpn "273.15:-:9:*:5:/:32:+" -text farenheit.txt
-touch farenheit.txt
-cmp farenheit.txt data/ref_farenheit.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
+../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -match ":TMP:0.01" -rpn "273.15:-:9:*:5:/:32:+" -text fahrenheit.txt
+touch fahrenheit.txt
+cmp fahrenheit.txt data/ref_fahrenheit.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
 
 echo "*** Takes relative humidity values, limits them to 100 and outputs to grib file"
 ../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -match ":RH:" -if ":RH:" -rpn "100:min" -fi -grib_out rh.grb -not_if ":RH:" -grib rh.grb
