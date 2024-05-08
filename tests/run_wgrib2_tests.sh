8c964da35807144c32e968a5b8551597060e5b28
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-08 10:21:22 -0600

    more tests

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -13,18 +13,17 @@ ls -l ../wgrib2
 ../wgrib2/wgrib2 > out.txt  && exit 1
 [ $? -ne 8 ] && exit 2
 
-# Run an inventory on a test file. This does not work.
-#echo "*** testing inventory of gdaswave.t00z.wcoast.0p16.f000.grib2..."
+echo "*** testing inventory of gdaswave.t00z.wcoast.0p16.f000.grib2..."
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 > tmp.txt
 cat tmp.txt
 cmp tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
 
-# Returns number of grid points in grid
+echo "*** Testing calculation of number of grid points"
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -npts > npts.txt
 cat npts.txt
 cmp npts.txt data/ref_npts_gdaswave.t00z.wcoast.0p16.f000.grib2.txt
 
-# Tests the calculation of wind speed, direction, and UGRD & VGRD components
+echo "*** Testing calculation of wind speed, direction, and UGRD & VGRD components"
 ../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -wind_dir wind.grb -wind_speed wind.grb -match "(UGRD|VGRD)"
 ../wgrib2/wgrib2 wind.grb > wind.txt
 cat wind.txt
@@ -34,10 +33,25 @@ cmp wind.txt data/ref_wind.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
 cat uv.txt
 cmp uv.txt data/ref_uv.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
 
-# Tests printing of grid information
+echo "*** Testing grid information"
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -grid > grid_test.txt
 cat grid_test.txt
 cmp grid_test.txt data/ref_grid.gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+
+echo "*** Testing returning contents of section 0"
+../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -Sec0 -grib_out sec0.grb
+../wgrib2/wgrib2 sec0.grb > sec0.txt
+cat sec0.txt
+cmp sec0.txt data/ref_sec0.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
+
+echo "*** Testing checksum on section 0"
+chksum0='../wgrib2/wgrib2 sec0.grb -checksum -1'
+chksum1='../wgrib2/wgrib2 data/ref_sec0.gdas.t12z.pgrb2.1p00.anl.75r.grib2 -checksum 0'
+
+if [ "$cksum0" != "$cksum1" ] ; then
+    echo "checksum failed"
+    exit 1
+fi
 
 echo "*** SUCCESS!"
 exit 0
