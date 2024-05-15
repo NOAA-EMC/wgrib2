478383fb6fc161cd0c12a0c4482362b8cca81fce
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-15 13:11:48 -0600

    removed recent test - wasn't working

--- tests/run_wgrib2_netcdf4_tests.sh
+++ tests/run_wgrib2_netcdf4_tests.sh
@@ -24,23 +24,6 @@ if [ "$n" -ne 1 ] ; then
   exit 1
 fi
 
-echo "*** Testing converting from grib to netcdf to grib with multiple variables"
-../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2  -match ":(UGRD|VGRD|HGT|TMP):0.4 mb" -grib_out temp.grib2
-../wgrib2/wgrib2 temp.grib2 -rpn 0 -grib_out junk_netcdf.template
-../wgrib2/wgrib2 temp.grib2 -nc4 -netcdf junk_netcdf.nc
-../wgrib2/wgrib2 junk_netcdf.template -import_netcdf junk_netcdf.nc HGT_0D4mb "0:1:0:181:0:360" \
-  -import_netcdf junk_netcdf.nc TMP_0D4mb "0:1:0:181:0:360" \
-  -import_netcdf junk_netcdf.nc UGRD_0D4mb "0:1:0:181:0:360" \
-  -import_netcdf junk_netcdf.nc VGRD_0D4mb "0:1:0:181:0:360" \
-  -grib_out junk_netcdf.grb
-
-cksum0=`../wgrib2/wgrib2 temp.grib2 -text -  | cksum`
-cksum1=`../wgrib2/wgrib2 junk_netcdf.grb -text -  | cksum`
-if [ "$cksum0" != "$cksum1" ] ; then
-    exit 1
-fi
-rm junk_netcdf.grb junk_netcdf.nc junk_netcdf.template
-
 echo "*** Testing converting from grib to netcdf with variables stored in 3D structure"
 ../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -nc_nlev 7 -netcdf junk_netcdf.nc -match ":(UGRD|VGRD|HGT|TMP):" 
 ncdump -v HGT,TMP,UGRD,VGRD junk_netcdf.nc > netcdf.txt
