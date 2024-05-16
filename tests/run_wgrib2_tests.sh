5e120e84ce32551c25c85caa4c70d6b4489d6f5d
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-16 12:59:58 -0600

    attempting to fix file comparison issue

--- tests/run_wgrib2_tests.sh
+++ tests/run_wgrib2_tests.sh
@@ -15,51 +15,51 @@ ls -l ../wgrib2
 
 echo "*** testing inventory of gdaswave.t00z.wcoast.0p16.f000.grib2..."
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 > tmp.txt
-cat tmp.txt
-cmp tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
+touch tmp.txt
+diff tmp.txt data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv
 
 echo "*** Testing calculation of number of grid points"
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -npts > npts.txt
-cat npts.txt
-cmp npts.txt data/ref_npts_gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+touch npts.txt
+diff npts.txt data/ref_npts_gdaswave.t00z.wcoast.0p16.f000.grib2.txt
 
 echo "*** Testing calculation of wind speed, direction, and UGRD & VGRD components"
 ../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -wind_dir wind.grb -wind_speed wind.grb -match "(UGRD|VGRD)"
 ../wgrib2/wgrib2 wind.grb > wind.txt
-cat wind.txt
-cmp wind.txt data/ref_wind.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
+touch wind.txt
+diff wind.txt data/ref_wind.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
 ../wgrib2/wgrib2 wind.grb -wind_uv uv.grb
 ../wgrib2/wgrib2 uv.grb > uv.txt
-cat uv.txt
-cmp uv.txt data/ref_uv.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
+touch uv.txt
+diff uv.txt data/ref_uv.gdas.t12z.pgrb2.1p00.anl.75r.grib2.inv
 
 echo "*** Testing grid information"
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -grid > grid_test.txt
-cat grid_test.txt
-cmp grid_test.txt data/ref_grid.gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+touch grid_test.txt
+diff grid_test.txt data/ref_grid.gdaswave.t00z.wcoast.0p16.f000.grib2.txt
 
 echo "*** Testing Sec0"
 ../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -Sec0 -grib_out sec0.grb
 ../wgrib2/wgrib2 sec0.grb > sec0.txt
-cat sec0.txt
-cmp sec0.txt data/ref_sec0.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
+touch sec0.txt
+diff sec0.txt data/ref_sec0.gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
 
 echo "*** Testing checksum on section 0"
 ../wgrib2/wgrib2 sec0.grb -checksum 0 > chksum1.txt
 ../wgrib2/wgrib2 data/ref_sec0.gdas.t12z.pgrb2.1p00.anl.75r.grib2 -checksum 0 > chksum2.txt
 touch chksum1.txt
 touch chksum2.txt
-cmp chksum1.txt chksum2.txt
+diff chksum1.txt chksum2.txt
 
 echo "*** Testing sec_len"
 ../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -Sec_len > sec_len.txt
-cat sec_len.txt
-cmp sec_len.txt data/ref_sec_len.gdaswave.t00z.wcoast.0p16.f000.grib2.txt
+touch sec_len.txt
+diff sec_len.txt data/ref_sec_len.gdaswave.t00z.wcoast.0p16.f000.grib2.txt
 
 echo "*** Testing sec_len on a small file"
 ../wgrib2/wgrib2 data/ref_simple_packing.grib2 -Sec_len > sec_len_small.txt
-cat sec_len_small.txt
-cmp sec_len_small.txt data/ref_sec_len.simple_packing.grib2.txt
+touch sec_len_small.txt
+diff sec_len_small.txt data/ref_sec_len.simple_packing.grib2.txt
 
 echo "*** Testing separating grib messages into separate files then recombining into single file"
 # creating comparison file using match 
@@ -104,7 +104,7 @@ fi
 echo "*** Testing spread output"
 ../wgrib2/wgrib2 data/ref_simple_packing.grib2 -v2 -spread spread.txt
 touch spread.txt
-cmp data/ref_simple_packing.grib2.spread.txt spread.txt
+diff data/ref_simple_packing.grib2.spread.txt spread.txt
 
 echo "*** Testing write/read section"
 ../wgrib2/wgrib2 data/ref_simple_packing.grib2 -write_sec 0 sec0.dat -write_sec 1 sec1.dat \
