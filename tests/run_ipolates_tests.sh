0c74c10be79e871e04765144174d16a4db0c320f
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-05-20 12:47:05 -0600

    testing commands in CI

--- tests/run_ipolates_tests.sh
+++ tests/run_ipolates_tests.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-# This checks the use of ip external library, only available if USE_IPOLATES is turned on in CMake.
+# This checks the use of ip external library, only available if USE_IPOLATES=1 CMake.
 #
 # Alyson Stahl 5/9/2024
 
 set -e
+
+../wgrib2/wgrib2.exe data/tmp_int.grb -new_grid_winds earth  -new_grid latlon 0:359:1 0:90:1 OUT.grb
+echo "*** SUCCESS!"
+exit 0
