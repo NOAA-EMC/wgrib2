0aa4400cd18b0adc9aebfd962f0c27edd35ed399
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-07-05 10:45:40 -0700

    update test

--- tests/run_ipolates_tests.sh
+++ tests/run_ipolates_tests.sh
@@ -5,7 +5,11 @@
 
 set -e
 
-../wgrib2/wgrib2 data/ref_simple_packing.grib2 -new_grid_winds earth  -new_grid latlon 0:359:1 0:90:1 OUT.grb
+../wgrib2/wgrib2 data/gdas.t12z.pgrb2.1p00.anl.75r.grib2 -match "(UGRD|VGRD)" -new_grid_winds grid  \
+    -new_grid latlon 0:359:1 0:90:1 new_grid.grb
+../wgrib2/wgrib2 new_grid.grb -grid -v2 > new_grid.txt
+touch new_grid.txt
+diff -w new_grid.txt data/ref_new_grid_gdas.t12z.pgrb2.1p00.anl.75r.grib2.txt
 
 echo "*** SUCCESS!"
 exit 0
