034795be5051cacba69d5b8e58a6ab3dc43bb8f1
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-07-12 13:33:45 -0700

    Update package.py

--- spack/package.py
+++ spack/package.py
@@ -189,7 +189,7 @@ class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
 
 class MakefileBuilder(spack.build_systems.makefile.MakefileBuilder):
     # Disable parallel build
-    parallel = True
+    parallel = False
 
     flag_handler = inject_flags
 
