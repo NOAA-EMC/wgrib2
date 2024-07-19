85183d9363e8f8306036f968735434ec2d91eb1b
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-07-19 12:24:50 -0700

    Update package.py

--- spack/package.py
+++ spack/package.py
@@ -89,6 +89,13 @@ class Wgrib2(MakefilePackage, CMakePackage):
         default="3",
         description="Use to interpolate to new grids (0 = OFF, 1 = ip, 3 = ip2)",
         values=("0", "1", "3"),
+        when="@:3.2"
+    )
+    variant(
+        "ipolates",
+        default=False,
+        description="Use ipolates library",
+        when="@3.3:"
     )
     variant(
         "spectral", default=False, description="Spectral interpolation in -new_grid", when="@:3.1"
@@ -153,7 +160,7 @@ class Wgrib2(MakefilePackage, CMakePackage):
     depends_on("wget", type=("build"), when="@:3.1 +netcdf4")
     depends_on("ip@:3", when="@3.2 ipolates=1")
     depends_on("ip2", when="@3.2 ipolates=3")
-    depends_on("ip@4.1:", when="@develop ipolates=1")
+    depends_on("ip@5.1:", when="@develop +ipolates")
     depends_on("libaec@1.0.6:", when="@3.2: +aec")
     depends_on("netcdf-c", when="@3.2: +netcdf4")
     depends_on("jasper@:2", when="@3.2: +jasper")
