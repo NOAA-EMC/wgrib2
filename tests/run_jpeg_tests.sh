7e10ea5ae41a543e5da5d20306b55d3212703bcd
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-07-26 12:32:17 -0700

    cmake support for openjpeg

---
+++ tests/run_jpeg_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This tests support for jpeg2000 compressed grib files.
+# Only available if USE_JASPER=ON or USE_OPENJPEG=ON CMake.
+#
+# Alyson Stahl 7/26/2024
+
+set -e
+
+echo "*** SUCCESS!"
+exit 0
