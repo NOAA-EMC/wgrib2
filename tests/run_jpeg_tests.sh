bf6d59f3c4839a7ae443d11b984b9769aec519ae
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-07-30 13:37:45 -0700

    swapped out test

--- tests/run_jpeg_tests.sh
+++ tests/run_jpeg_tests.sh
@@ -4,19 +4,11 @@
 #
 # Alyson Stahl 7/26/2024
 
-set -e
-echo "*** Running jpeg tests"
-
-echo "create a new grib file with integer values"
-../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -rpn floor -set_scaling 0 0 -set_grib_type same -grib_out jpeg.grb
-../wgrib2/wgrib2 jpeg.grb -v2 -s > jpeg.txt
-
-echo "*** Converting from jpeg to simple packing and back"
-../wgrib2/wgrib2 jpeg.grb -set_grib_type simple -grib_out jpeg2simple.grb
-../wgrib2/wgrib2 jpeg2simple.grb -set_grib_type jpeg -grib_out simple2jpeg.grb
-../wgrib2/wgrib2 simple2jpeg.grb -v2 -s > simple2jpeg.txt
-touch simple2jpeg.txt 
-diff -w simple2jpeg.txt jpeg.txt
+echo "*** Converting from jpeg to simple packing"
+../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -set_grib_type simple -grib_out jpeg2simple.grb
+../wgrib2/wgrib2 jpeg2simple.grb -v2 -s >  jpeg2simple.txt
+touch jpeg2simple.txt
+diff -w jpeg2simple.txt data/ref_jpeg2simple.txt
 
 echo "*** SUCCESS!"
 exit 0
