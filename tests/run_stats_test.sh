17c302485035484ee204fd1c4f39357040945e71
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-03-07 12:29:43 -0800

    viewing stats results for different file types on CI

---
+++ tests/run_stats_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+echo ""
+
+echo "*** JPEG File"
+../wgrib2/wgrib2 LARGECAT220250305_12_1443copy.grib2 -stats
+
+echo "**********************************"
+echo "*** PNG File"
+../wgrib2/wgrib2 data/large_png.grb2 -stats
+
+
+echo "**********************************"
+echo "*** Simple File"
+../wgrib2/wgrib2 data/ref_simple_packing.grib2 -stats
+
+echo "*** SUCCESS!"
+exit 0
