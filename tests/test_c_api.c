a4b2e313e36a9d769aae1eedb6e07746bd8dd932
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-07-02 11:59:46 -0700

    more debugging test_c_api

--- tests/test_c_api.c
+++ tests/test_c_api.c
@@ -24,7 +24,7 @@ extern jmp_buf fatal_err;
 int
 main()
 {
-    /*
+    
     printf("Testing grb2_mk_inv()...\n");
     {
         int ret;
@@ -41,6 +41,7 @@ main()
     }
         
     printf("ok!\n");
+    /*
     printf("Testing wgrib2_add_cmd()...\n");
     printf("Testing with overly long command string...\n");
     {
