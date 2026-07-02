34322eb88e25d819549a020976abd193ed326424
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-07-02 12:05:04 -0700

    Update test_c_api.c

--- tests/test_c_api.c
+++ tests/test_c_api.c
@@ -41,7 +41,6 @@ main()
     }
         
     printf("ok!\n");
-    /*
     printf("Testing wgrib2_add_cmd()...\n");
     printf("Testing with overly long command string...\n");
     {
@@ -57,7 +56,6 @@ main()
         }
     }
     printf("ok!\n");
-    */
     printf("Testing with too many options...\n");
     {
         int i;
