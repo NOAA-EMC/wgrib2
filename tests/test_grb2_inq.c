28bf99c03d17991eb3ed9432d4d927e408569774
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-07-17 12:57:23 -0700

    add tests for grb2_get_lonlat()

--- tests/test_grb2_inq.c
+++ tests/test_grb2_inq.c
@@ -68,7 +68,63 @@ main()
             printf("ERROR: grb2_get_data() returned %d, expected 0.\n", ret);
             return 5;
         }
-    }    
+    }
+    printf("Testing grb2_get_lonlat()...\n");
+    {
+        int ret;
+        int last_options, good, npnts;
+        int ndata = NDATA;
+        float lon[NDATA] = { 0. };
+        float lat[NDATA] = { 0. };
+
+        /* Set registers 17 and 18 with longitude and latitude data. */
+        wgrib2_set_reg(lon, (size_t)ndata, 17);
+        wgrib2_set_reg(lat, (size_t)ndata, 18);
+
+        /* Reset all from last tests. */
+        last_options = 0;
+        good = 0;
+        npnts = 0;
+
+        grb2_inq_set_state(last_options, good, npnts);
+
+        /* Last find failed (good = 0), so should return 10. */
+        ret = grb2_get_lonlat(lon, lat, ndata);
+        if (ret != 10) {
+            printf("ERROR: grb2_get_lonlat() returned %d, expected 10.\n", ret);
+            return 6;
+        }
+
+        /* Wrong size data (ndata != npnts). Should return 11. */
+        good = 1;
+        grb2_inq_set_state(last_options, good, npnts);
+        ret = grb2_get_lonlat(lon, lat, ndata);
+        if (ret != 11) {
+            printf("ERROR: grb2_get_lonlat() returned %d, expected 11.\n", ret);
+            return 7;
+        }
+
+        /* Invalid options (reading lonlat not requested). Should return 12. */
+        good = 1;
+        npnts = ndata;
+        grb2_inq_set_state(last_options, good, npnts);
+        ret = grb2_get_lonlat(lon, lat, ndata);
+        if (ret != 12) {
+            printf("ERROR: grb2_get_lonlat() returned %d, expected 12.\n", ret);
+            return 8;
+        }
+
+        /* Valid Case. Should return 0. */
+        last_options = LONLAT;
+        good = 1;
+        npnts = ndata;
+        grb2_inq_set_state(last_options, good, npnts);
+        ret = grb2_get_lonlat(lon, lat, ndata);
+        if (ret != 0) {
+            printf("ERROR: grb2_get_lonlat() returned %d, expected 0.\n", ret);
+            return 9;
+        }
+    }
     printf("SUCCESS!\n");
     return 0;
 }
