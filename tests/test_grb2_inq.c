f843a58acb5dc24abd084025aeb49ab461fa21b7
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-07-17 10:42:06 -0700

    initial test set up

---
+++ tests/test_grb2_inq.c
@@ -0,0 +1,26 @@
+/* This is a test of the functions in grb2_inq.c, which is part of the C API for wgrib2.
+ * 
+ * Alyson Stahl
+*/
+
+#include "c_wgrib2api.h"
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define NDATA 100
+
+int
+main()
+{
+    printf("Testing grb2_get_data()...\n");
+    {
+        int ret;
+        int ndata = NDATA;
+        float data[NDATA] = { 0. };
+
+
+    }
+    printf("SUCCESS!\n");
+    return 0;
+}
